bc5cf7060ad70ef0f567a7ccc8a1c52989873440
Zimin0 <you@example.com>
2023-12-28 11:47:16 +0300

    Add models Item, Order, Discount, Tax

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3)
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = sum(item.price for item in self.items.all())
+        super(Order, self).save(*args, **kwargs)
+
+class Discount(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    discount_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Tax(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    tax_amount = models.DecimalField(max_digits=10, decimal_places=2)
