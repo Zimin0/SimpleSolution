4abb38b243ffb7171904f301736dbc713dd5ef9e
Zimin0 <you@example.com>
2023-12-28 21:34:32 +0300

    Add localization of models

--- payments/models.py
+++ payments/models.py
@@ -1,17 +1,36 @@
 from django.db import models
 
 class Item(models.Model):
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
     CURRENCY = (
         ("RUB", "Рубли"),
         ("EUR", "Евро"),
     )
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    currency = models.CharField(max_length=3, choices=CURRENCY)
+
+    name = models.CharField(verbose_name="Название", max_length=200)
+    description = models.TextField(verbose_name="Описание")
+    price = models.DecimalField(verbose_name="Цена", max_digits=10, decimal_places=2)
+    currency = models.CharField(verbose_name="Валюта", max_length=3, choices=CURRENCY)
+
+    def __str__(self):
+        return f"{self.name}"
 
 class Order(models.Model):
-    items = models.ManyToManyField(Item)
+    class Meta:
+        verbose_name = "Заказ на покупку"
+        verbose_name_plural = "Заказы на покупку"
+
+    items = models.ManyToManyField(Item, verbose_name="Товары в заказе")
+    
+    def __str__(self):
+        return f"{self.id}"
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.calculate_total_price()
+        super(Order, self).save(*args, **kwargs)
 
     def calculate_total_price(self):
         if self.pk:
@@ -25,14 +44,25 @@ class Order(models.Model):
         else:
             return 0
 
-    def save(self, *args, **kwargs):
-        self.total_price = self.calculate_total_price()
-        super(Order, self).save(*args, **kwargs)
 
 class Discount(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    discount_amount = models.DecimalField(max_digits=10, decimal_places=2)  # процент скидки
+    class Meta:
+        verbose_name = "Скидка на заказ"
+        verbose_name_plural = "Скидки на заказ"
+    
+    def __str__(self):
+        return f"{self.id}"
+
+    order = models.ForeignKey(Order, verbose_name="Заказ", on_delete=models.CASCADE)
+    discount_amount = models.DecimalField(verbose_name="Скидка(%)", max_digits=5, decimal_places=2) 
 
 class Tax(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    tax_amount = models.DecimalField(max_digits=10, decimal_places=2)  # процент налога
+    class Meta:
+        verbose_name = "Налог на заказ"
+        verbose_name_plural = "Налоги на заказ"
+    
+    def __str__(self):
+        return f"{self.id}"
+
+    order = models.ForeignKey(Order, verbose_name="Заказ", on_delete=models.CASCADE)
+    tax_amount = models.DecimalField(verbose_name="Скидка(%)", max_digits=10, decimal_places=2)
