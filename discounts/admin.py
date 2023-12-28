f17a9b8c05b1281ee885b3bfb5d2bfca298bf924
Zimin0 <you@example.com>
2023-12-28 22:09:38 +0300

    Add search_fields, list_filter and so on to ModelAdmin

--- discounts/admin.py
+++ discounts/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from discounts.models import Discount, Tax
 
 @admin.register(Discount)
-class Discount(admin.ModelAdmin):
+class DiscountAdmin(admin.ModelAdmin):
     list_display = ['order', 'discount_amount']
+    search_fields = ['order__id']  
+    list_filter = ['order']  
+    ordering = ['order'] 
 
 @admin.register(Tax)
 class TaxAdmin(admin.ModelAdmin):
     list_display = ['order', 'tax_amount']
+    search_fields = ['order__id']  
+    list_filter = ['order']  
+    ordering = ['order']  
--- payments/admin.py
+++ payments/admin.py
@@ -4,11 +4,17 @@ from payments.models import Item, Order
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'price', 'currency']
+    search_fields = ['name', 'currency'] 
+    list_filter = ['currency'] 
+    ordering = ['name']  
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'get_items', 'calculate_total_price']
+    
     def get_items(self, obj):
         return ", ".join([item.name for item in obj.items.all()])
     get_items.short_description = 'Items'
 
+    search_fields = ['id']  
+    ordering = ['id']  
