dbe7c4d32eaac772498eb1aa32d9b07bbe162793
Zimin0 <you@example.com>
2023-12-28 13:18:04 +0300

    Add displaying models in admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Item, Order, Discount, Tax
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'currency']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['get_items', 'total_price']
+    def get_items(self, obj):
+        return ", ".join([item.name for item in obj.items.all()])
+    get_items.short_description = 'Items'
+
+@admin.register(Discount)
+class Discount(admin.ModelAdmin):
+    list_display = ['order', 'discount_amount']
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ['order', 'tax_amount']
