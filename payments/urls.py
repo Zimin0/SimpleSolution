65b188b252f44efca6088684b8616ab3f53ced3f
Zimin0 <you@example.com>
2023-12-28 21:16:33 +0300

    Add require_GET, app_name

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from payments.views import buy_item, item_detail, pay_order
 
+app_name = 'payments'
+
 urlpatterns = [
     path('buy/<int:id>/', buy_item, name='buy_item'),
     path('item/<int:id>/', item_detail, name='item_detail'),
--- payments/views.py
+++ payments/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.http import JsonResponse
 from payments.models import Item, Order
 from payments.business.stripe_utils import StripeSessionCreator
+from django.views.decorators.http import require_POST, require_GET
 
+@require_GET
 def buy_item(request, id):
     item = get_object_or_404(Item, pk=id)
 
@@ -20,10 +22,12 @@ def buy_item(request, id):
 
     return JsonResponse({'session_id': session.id})
 
+@require_GET
 def item_detail(request, id):
     item = get_object_or_404(Item, pk=id)
     return render(request, 'payments/item_detail.html', {'item': item})
 
+@require_GET
 def pay_order(request, order_id):
     order = get_object_or_404(Order, pk=order_id)
 
