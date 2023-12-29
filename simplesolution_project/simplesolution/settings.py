2711e3d074eb56109ddb1bcf3cf4e7427b6d1d50
Zimin0 <you@example.com>
2023-12-29 10:56:14 +0300

    Add dotenv lib to handle env vars

--- simplesolution_project/simplesolution/settings.py
+++ simplesolution_project/simplesolution/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,9 +86,9 @@ WSGI_APPLICATION = 'simplesolution.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),  
-        'USER': os.environ.get('POSTGRES_USER'),      
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'), 
+        'NAME': os.getenv('POSTGRES_DB'),  
+        'USER': os.getenv('POSTGRES_USER'),      
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'), 
         'HOST': 'db',
         'PORT': '5432',
     }
